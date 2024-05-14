3ad47b7c43bbc2d2ca0b720227e5b081cb2b2b69
Archi <92038722+archi243@users.noreply.github.com>
2024-05-14 16:43:23 +0530

    script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==========================================typed js=======================================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'AI Expert'],
+    strings: ['Frontend Developer', 'AI Enthusiast', 'Cybersecurity Enthusiast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
